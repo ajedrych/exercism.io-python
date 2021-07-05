32156c06dd8610d6668632dde2782014405aa4a5
ajedrych <jedrych.al@gmail.com>
2021-07-05 11:58:19 +0200

    resistor-color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,12 @@
+resistors = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
+        "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+
+def color_code(color):
+    return resistors[color]
+    pass
+
+
+def colors():
+    return ["black", "brown", "red", "orange", "yellow",
+        "green", "blue", "violet", "grey", "white"]
+    pass
