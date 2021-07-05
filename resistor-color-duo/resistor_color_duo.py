03773115bc9c1bf28c7708e979399e140eb655cd
ajedrych <jedrych.al@gmail.com>
2021-07-05 12:04:41 +0200

    resistor-color-duo

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,7 @@
+resistors = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
+        "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+
+def value(colors):
+    
+    return resistors[colors[0]] * 10 + resistors[colors[1]]
+    pass
