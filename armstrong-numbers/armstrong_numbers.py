43d7c449326581891a0ca026a837fcbff72c50db
ajedrych <jedrych.al@gmail.com>
2021-07-06 14:20:10 +0200

    armstrong-numbers

---
+++ armstrong-numbers/armstrong_numbers.py
@@ -0,0 +1,10 @@
+def is_armstrong_number(number):
+    number_converted = list(map(int, str(number)))
+    sum = 0
+
+    for x in number_converted:
+        sum += x**len(number_converted)
+    
+    if sum == number:
+        return True
+    else: return False
