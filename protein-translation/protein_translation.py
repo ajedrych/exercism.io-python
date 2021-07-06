12c6fbad2738510d23f7d9705fbfd32a8472e053
ajedrych <jedrych.al@gmail.com>
2021-07-06 16:13:47 +0200

    protein-translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,29 @@
+from textwrap import wrap
+
+amino = {'AUG':'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'}
+
+def proteins(strand):
+    codons = wrap(strand, 3)
+    sequence = []
+    for x in codons:
+        protein = amino[x]
+        if protein == 'STOP':
+            break
+        sequence.append(protein)
+    return sequence
