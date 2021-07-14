88c4016d6c8cb8b7be7ed2963b2bdf070d23f53e
ajedrych <jedrych.al@gmail.com>
2021-07-14 13:27:53 +0200

    difference_of_squares

---
+++ difference-of-squares/difference_of_squares.py
@@ -0,0 +1,25 @@
+from typing import Counter
+
+
+def square_of_sum(number):
+    counter_square_of_sum = 0
+    for i in range(1, number + 1):
+        counter_square_of_sum += i
+    return counter_square_of_sum ** 2
+
+
+def sum_of_squares(number):
+    counter_sum_of_squares = 0
+    for i in range(number + 1):
+        counter_sum_of_squares += i ** 2
+    return counter_sum_of_squares
+
+
+def difference_of_squares(number):
+    counter_square_of_sum = 0
+    counter_sum_of_squares = 0
+    for i in range(number + 1):
+        counter_square_of_sum += i
+        counter_sum_of_squares += i ** 2
+    return (counter_square_of_sum ** 2) - counter_sum_of_squares
+    
