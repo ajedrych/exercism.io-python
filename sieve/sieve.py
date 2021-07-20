09f4842cf25661666a2c4a5cc8f0cc91a974b0a7
ajedrych <jedrych.al@gmail.com>
2021-07-20 17:54:49 +0200

    sieve

---
+++ sieve/sieve.py
@@ -0,0 +1,13 @@
+from typing import final
+
+
+def primes(limit):
+    numbers = []
+    final = []
+    for i in range(2, limit + 1):
+        if i not in numbers:
+            final.append(i)
+            for j in range (i*i, limit + 1, i):
+                numbers.append(j)
+    
+    return final
