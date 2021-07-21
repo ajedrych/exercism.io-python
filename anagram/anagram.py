1b4c401b4c434cb84b3f7b8cec186dac7b586290
ajedrych <jedrych.al@gmail.com>
2021-07-21 15:50:38 +0200

    anagram

---
+++ anagram/anagram.py
@@ -0,0 +1,10 @@
+def find_anagrams(word, candidates):
+    expected = []
+
+
+    for words in candidates:
+        if word.lower() != words.lower():
+            if sorted(word.lower()) == sorted(words.lower()):
+                expected.append(words)
+
+    return expected
