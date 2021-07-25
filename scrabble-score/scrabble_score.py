3e77f2ec34f4ac18d79e4a0579877a3ddf4c86f0
ajedrych <jedrych.al@gmail.com>
2021-07-25 22:52:39 +0200

    update scrabble score after feedback from mentor

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,17 +1,18 @@
 def score(word):
-    points = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-        'D': 2, 'G': 2,
-        'B': 3, 'C': 3, 'M': 3, 'P': 3,
-        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    points = {'AEIOULNRST' : 1,
+        'DG' : 2,
+        'BCMP': 3,
+        'FHVWY': 4,
         'K': 5,
-        'J': 8, 'X': 8,
-        'Q': 10, 'Z': 10}
+        'JX': 8,
+        'QZ': 10}
 
     word = word.upper()
     counter = 0
 
     for letter in word:
-        counter += points.get(letter)
-
+        for i in points.keys():
+            for j in i:
+                if j == letter:
+                    counter += points.get(i)
     return counter
-
