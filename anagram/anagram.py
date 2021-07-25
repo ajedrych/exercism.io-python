180188e0a11441065c76cb2525c0d8771e5bc239
ajedrych <jedrych.al@gmail.com>
2021-07-25 22:49:16 +0200

    a

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,7 +1,6 @@
 def find_anagrams(word, candidates):
     expected = []
 
-
     for words in candidates:
         if word.lower() != words.lower():
             if sorted(word.lower()) == sorted(words.lower()):
