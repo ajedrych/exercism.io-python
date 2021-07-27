5c93d29dca92bf745320ee4c22ffee68c425e1bd
ajedrych <jedrych.al@gmail.com>
2021-07-27 13:37:17 +0200

    secret handshake

---
+++ secret-handshake/secret_handshake.py
@@ -0,0 +1,19 @@
+def commands(number):
+    number = bin(number)
+    number = number.replace("b", "0")
+    print(number)
+    result = []
+
+    if number[-1] == "1":
+        result.append("wink")
+    if number[-2] == "1":
+        result.append("double blink")
+    if len(number) > 3 and number[-3] == "1":
+        result.append("close your eyes")
+    if len(number) > 4 and number[-4] == "1":
+        result.append("jump")
+    if len(number) > 5 and number[-5] == "1":
+        result.reverse()
+    print(result)
+    return result
+
